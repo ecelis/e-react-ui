1c15515dcf058058f7f5ed9f1e9d56087fe0b2ff
Ernesto Celis <ernesto.celis@enroutesystems.com>
2022-08-07 21:51:18 -0500

    Set properties to buttons and pill

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,13 +8,16 @@ import styled from 'styled-components';
  */
 const BaseButton = styled.button`
   /* Adapt the colors based on primary prop */
+  height: fit-content;
+  max-height: 150px;
+  width: fit-content;
+  max-width: 300px;
   background: ${props => props.primary ? "var(--primary)" : "var(--light)"};
   color: ${props => props.primary ? "var(--light)" : "var(--primary)"};
-
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid #6667ab;
+  border: 2px solid var(--primary);
   border-radius: 3px;
   &:active {
     transition: all 180ms ease-in-out;
@@ -49,32 +52,38 @@ Button.propTypes = {
 
 const BasePill = styled(Link)`
   display: inline-block;
+  height: fit-content;
+  max-height: 150px;
+  width: fit-content;
+  max-width: 300px;
   border-radius: 3px;
   padding: 3px;
   margin: 3px;
   width: 5rem;
-  background: ${props => props.selected ? "var(--primary)" : "lightgrey"};
-  color: ${props => props.selected ? "whitesmoke" : "var(--primary)"};
-  border: ${props => props.selected ? "thin solid var(--primary)" : "thin solid grey"};
+  background: ${props => props.selected ? "var(--primary)" : "var(--light)"};
+  color: ${props => props.selected ? "var(--light)" : "var(--primary)"};
+  border: ${props => props.selected ? "thin solid var(--primary)" : "thin solid var(--light)"};
   border-radius: 32px;
   font-size: 12px;
   text-align: center;
   text-decoration: none;
   &:active {
     transition: all 180ms ease-in-out;
-    background: ${props => props.primary ? "whitesmoke" : "var(--primary)"};
-    color: ${props => props.primary ? "var(--primary)" : "whitesmoke"};
+    background: ${props => props.primary ? "var(--light)" : "var(--primary)"};
+    color: ${props => props.primary ? "var(--primary)" : "var(--light)"};
   }
   &:hover {
-    background: ${props => props.primary ? "whitesmoke" : "var(--primary)"};
-    color: ${props => props.primary ? "var(--primary)" : "whitesmoke"};
+    background: ${props => props.primary ? "var(--light)" : "var(--primary)"};
+    color: ${props => props.primary ? "var(--primary)" : "var(--light)"};
   }
 `;
 
 export function Pill (props) {
   return (
-    <BasePill onClick={props.onClick} id={props.itemId}
-      to={props.link ? `${props.to}` : `${props.itemType},${props.itemId}`}
+    <BasePill
+      onClick={props.onClick}
+      id={props.id}
+      to={props.to ? `${props.to}` : `${props.itemType},${props.id}`}
       selected={props.selected}>
       {`${props.label}`}</BasePill>
   );
@@ -82,8 +91,8 @@ export function Pill (props) {
 
 Pill.propTypes = {
   onClick: PropTypes.func,
-  itemId: PropTypes.any.isRequired,
-  link: PropTypes.string.isRequired,
+  id: PropTypes.any.isRequired,
+  to: PropTypes.string.isRequired,
   itemType: PropTypes.any,
   selected: PropTypes.bool,
   label: PropTypes.string.isRequired
