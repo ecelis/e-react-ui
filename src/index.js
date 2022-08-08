0a2330757fe0c4e655fa4abd3aea05f42ef9fcc3
Ernesto Celis <ernesto.celis@enroutesystems.com>
2022-08-07 23:35:24 -0500

    Again the index

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Autocomplete from './components/Autocomplete';
+import { Button, Pill } from "./components/Button";
+import Calendar from './components/Calendar';
+import Carousel from "./components/Carousel";
+import Container from './components/Container';
+import GlobalStyle from "./components/globalStyles";
+import {
+    Col,
+    Grid,
+    Row
+} from './components/Grid';
+import List from './components/List';
+
+export {
+    Autocomplete,
+    Button,
+    Calendar,
+    Carousel,
+    Container,
+    Col,
+    GlobalStyle,
+    Grid,
+    List,
+    Pill,
+    Row
+};
