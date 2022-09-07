41203eb9041936c39881eb18e6170ea9b5c9d722
Ernesto Celis <ernesto.celis@enroutesystems.com>
2022-09-06 21:32:50 -0500

    Accept children for Pill.
    Fix sizing for container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
-    height: ${props => props.height ? props.height : 106}px;
+    min-height: fit-content;
+    height: ${props => props.height ? `${props.height};` : null};
+    width: ${props => props.width ? `${props.width};` : 375}px;
     overflow: ${props => props.overflow ? `${props.overflow};` : null};
     ${props => props.background ? `background: ${props.background};` : `color: inherit;`}
     ${props => props.border ? `border: ${props.border};` : null}
     ${props => props.color ? `color: ${props.color};` : `color: inherit;`}
+    ${props => props.justify ? `justify-content: ${props.justify};` : null }
     
 `;
 
@@ -17,6 +20,8 @@ Container.propTypes = {
     border: PropTypes.string,
     overflow: PropTypes.string,
     height: PropTypes.string,
+    width: PropTypes.string,
+    justify: PropTypes.string,
 }
 
 export default Container;
